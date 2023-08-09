fbba44545d66544cbf4937c37afab44f07d6f947
Kleyton Barcelos <kleytonbarcelos@gmail.com>
2023-08-09 14:31:16 -0300

    mudando o code pelo chatGPT

--- index.js
+++ index.js
@@ -1,83 +1,64 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 import scrape from 'website-scraper';
-//import defaultOptions from 'website-scraper/defaultOptions';
-import SaveToExistingDirectoryPlugin from 'website-scraper-existing-directory';
-import { zip } from 'zip-a-folder';
-import fs from 'fs-extra'
+import archiver from 'archiver';
+import path from 'path';
+import fs from 'fs';
+import crypto from 'crypto';
 
-import express from 'express'
-const app = express()
-const port = 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/make/:url', (req, res) => {
-  let response = convert(req.params.url)
-  res.send('hello world: '+response)
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`>>> Example app listening on port ${port}`)
+app.post('/clonar', async (req, res) => {
+  const { url } = req.body;
 
-  convert('https://metodovidasaudavel.com/')
-})
+  // Gera um SKU aleatório
+  const sku = crypto.randomBytes(4).toString('hex');
+  const subDirectory = path.join('websites', sku);
 
-const convert = async (url) => {
-  console.log('==========================================================')
-  console.log('STARTING....')
-
-  const prettifyUrls = true
-  // let url = ''
-  // url = 'https://curcumy.info'
-  // url = 'https://curcumy.info/'
-  // url = 'https://paginadasaude.com.br/instituto-plano-de-menina-e-bayer-promovem-evento-gratuito-sobre-saude-feminina-na-share-student-living-butanta-neste-sabado/'
-  // url = 'https://www.g1.com.br'
-  // url = 'https://www.hola.com/actualidad/20230122225024/shakira-pique-hijo-milan-cumpleanos/'
-  //url = 'https://icloakerpro.com/site/'
-  // url = 'https://oliveirassomerville.com'
-
-  const folderSite = (Math.round((Math.pow(36, 10 + 1) - Math.random() * Math.pow(36, 10))).toString(36).slice(1))
-  const dir = './websites/'+folderSite
-
-  fs.mkdirSync(dir)
-
-  let response = await scrape({
+  // Configuração para o website-scraper
+  const options = {
     urls: [url],
-    directory: dir,
-    plugins: [ new SaveToExistingDirectoryPlugin() ],
-    // defaultFilename: 'home.html',
-    // filenameGenerator: 'bySiteStructure',
-    // urlFilter: function (url) {
-    //     return url.indexOf(websiteUrl) === 0;
-    // },
-    //recursive: true,
-    prettifyUrls,
-    // request: {
-    //     headers: {
-    //         'User-Agent': 'Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19'
-    //     }
-    // },
-    // subdirectories: [
-    // 	{ directory: 'assets/images', extensions: ['.png', '.jpg', '.jpeg', '.gif', '.webp'] },
-    // 	{ directory: 'assets/js', extensions: ['.js'] },
-    // 	{ directory: 'assets/css', extensions: ['.css'] },
-    // 	{ directory: 'assets/media', extensions: ['.mp4', '.mp3', '.ogg', '.webm', '.mov', '.wave', '.wav', '.flac'] },
-    // 	{ directory: 'assets/fonts', extensions: ['.ttf', '.woff', '.woff2', '.eot', '.svg'] }
-    // ],
-  })
+    directory: subDirectory,
+  };
 
   try {
-    if(response){
-      let zipFolder = new URL(url).hostname
-      let date = new Date(new Date().getTime() - (new Date().getTimezoneOffset() * 60000)).toISOString().split(".")[0].replace(/[T:]/g, '').replace(/[T-]/g, '')
-      let zipFile = './websites/'+zipFolder+'_'+date+'.zip'
-      zip(dir, zipFile)
-      console.log( "Entire website succesfully downloaded>> "+zipFile )
-      //zip.compressFolder(dir, './websites/'+zipFolder+'.zip');
-    }
+    await scrape(options);
+
+    // Cria um arquivo zip do conteúdo clonado
+    const output = fs.createWriteStream(`${subDirectory}.zip`);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+
+    output.on('close', async () => {
+      console.log('Arquivo zip criado:', archive.pointer(), 'bytes');
+      res.download(`${subDirectory}.zip`, async () => {
+        // Remove o arquivo zip após o download
+        fs.rmSync(`${subDirectory}.zip`);
+
+        // Remove a pasta gerada se ela existir
+        if (fs.existsSync(subDirectory)) {
+          fs.rmdirSync(subDirectory, { recursive: true });
+          console.log('Pasta removida:', subDirectory);
+        }
+      });
+    });
+
+    archive.on('error', (err) => {
+      throw err;
+    });
+
+    archive.pipe(output);
+    archive.directory(subDirectory, false);
+    archive.finalize();
   } catch (error) {
-    console.log("An error ocurred", error);
-  }  
-  console.log('END')
-  console.log('==========================================================')
-}
+    console.error('Erro ao clonar o site:', error.message);
+    res.status(500).send('Ocorreu um erro ao clonar o site.');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
